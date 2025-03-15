f6e68f407aa6c12f5571b60e743b02af6dd3fcf3
shamil256git <shamilbaker50@gmail.com>
2025-03-15 11:44:10 +0300

    commit #3
    in the NV on from line 1 to line 12 I created the Item class in the com.itemmanagement package,  Added a private itemName variable, Implemented a constructor to initialize itemName, Added a getItemInfo method to return item details

--- modB/src/com/itemmanagement/Item.java
+++ modB/src/com/itemmanagement/Item.java
@@ -1,15 +1,12 @@
 package com.itemmanagement;
-
 public class Item {
     private String itemName;
 
-    public Item (String itemName) {
+    public Item(String itemName) {
         this.itemName = itemName;
     }
-
     public String getItemInfo() {
         return "Item: " + itemName;
+
     }
 }
-
-
--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import com.itemmanagement.*;
 public class Main {
     public static void main(String[] args) {
         User user = new User("Abubaker", 25);
-        Item item = new Item("television");
+        Item item = new Item("Television");
 
         System.out.println(user.getUserInfo());
         System.out.println(item.getItemInfo());
